4e9bee88f6b6c211b46ce0cfa732441999dbc518
Pankaj Thakur <pankaj.thakur1651@gmail.com>
2024-05-05 17:21:25 +0200

    add new chapter for common programming concepts

---
+++ common_programming_concepts/src/main.rs
@@ -0,0 +1,37 @@
+mod sub;
+fn variable_and_mutability()
+{
+    println!("variables and mutability")
+}
+
+fn data_types()
+{
+    println!("data types")
+}
+
+fn functions()
+{
+    sub::first_func();
+    println!("functions")
+}
+
+fn comments()
+{
+    println!("comments")
+}
+
+
+fn control_flows()
+{
+    println!("control_flows")
+}
+
+fn main() {
+    println!("Hello, world!");
+    variable_and_mutability();
+    data_types();
+    functions();
+    comments();
+    control_flows();
+
+}
---
+++ common_programming_concepts/src/sub.rs
@@ -0,0 +1,10 @@
+pub fn first_func()
+{
+    println!("first func from other file");
+    second_func();
+}
+
+fn second_func()
+{
+    println!("second func from other file");
+}
